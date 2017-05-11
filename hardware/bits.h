8193bc4b9b1dd6b5f0b032423cbee5a33d3ca705
Mike Gore <magore@sympatico.ca>
2017-05-10 20:07:29 -0400

    sync fatfs fatfs.sup fatfs.hal lib printf hal.c hal.h bits.h with esp8266_ili9341 project

--- hardware/bits.h
+++ hardware/bits.h
@@ -4,14 +4,21 @@
  @brief Bit manipulation macros.
 
  @par Edit History
- - [1.0]   [Mike Gore]  Initial revision of file.
+ - [1.1]   [Mike Gore]  Initial revision of file.
 
- @par Copyright &copy; 2014 Mike Gore, Inc. All rights reserved.
+ @par Copyright &copy; 2014-2017 Mike Gore, Inc. All rights reserved.
 
 */
 
-///  Note: IF x and y are constants the compiler will fully reduce the expression
+#ifndef _BITS_H_
+#define _BITS_H_
 
+///  Note: IF x and y are constants the compiler will fully reduce the expression
+#define BIT_SET(x,y)    (x |=  (1 << (y)))
+#define BIT_CLR(x,y)    (x &= ~(1 << (y)))
+#define BIT_TST(x,y)    ((x  &  (1 << (y))) ? (int) 1 : (int) 0)
+
+#ifdef AVR
 #define BIT0    0
 #define BIT1    1
 #define BIT2    2
@@ -29,7 +36,7 @@
 #define BIT13   13
 #define BIT14   14
 #define BIT15   15
+#endif	// AVR
 
-#define BIT_SET(x,y)    ((x) |=  (1 << (y)))
-#define BIT_CLR(x,y)    ((x) &= ~(1 << (y)))
-#define BIT_TST(x,y)    (((x)  &  (1 << (y))) ? (int) 1 : (int) 0)
+
+#endif // ifndef _BITS_H_
